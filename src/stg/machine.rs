a441ca1eb574ac749c98fb47d6ed6fe93ca42ccd
wasabi315 <wasabi.nvim.st@gmail.com>
2021-06-28 19:58:33 +0900

    wip

--- src/stg/machine.rs
+++ src/stg/machine.rs
@@ -90,7 +90,7 @@ impl ToValue for String {
             .get(self)
             .or_else(|| globals.get(self))
             .map(Clone::clone)
-            .ok_or_else(|| -> Box<dyn Error> { Box::new(UnboundVariable(self.clone())) })
+            .ok_or_else(|| UnboundVariable(self.clone()).into())
     }
 }
 
@@ -109,25 +109,151 @@ impl ToValue for Atom {
     }
 }
 
-const RULES: &'static [fn(&mut State) -> Result<bool>] = &[
-    eval_var_app,
-    enter_non_updatable,
-    eval_let,
-    eval_case,
-    eval_constr_app,
-    eval_literal,
-    eval_literal_app,
-    eval_prim_app,
-];
+fn run(state: &mut State) -> Result<()> {
+    loop {
+        match &mut state.code {
+            Code::Eval {
+                expr: Expr::VarApp { var, args },
+                locals,
+            } => {
+                let args = ToValue::vals(args, locals, &state.globals)?;
+                match var.val(locals, &state.globals)? {
+                    Value::Addr(addr) => {
+                        state.code = Code::Enter(Rc::clone(&addr));
+                        prepend(&mut state.args, args);
+                    }
+                    Value::Int(n) => {
+                        if !args.is_empty() {
+                            return Err(ApplyToInt.into());
+                        }
+                        state.code = Code::ReturnInt(n);
+                    }
+                }
+            }
 
-fn transition(state: &mut State) -> Result<()> {
-    for transition in RULES.iter() {
-        if transition(state)? {
-            break;
-        }
+            Code::Eval {
+                expr: Expr::Let { rec, binds, expr },
+                locals,
+            } => {
+                // 1. allocate closures
+                let addrs = binds
+                    .0
+                    .values()
+                    .map(|lf| Rc::new(RefCell::new(Some(Closure { lf, free: vec![] }))));
+
+                // 2. create bindings
+                let mut next_locals = locals.clone();
+                for (var, addr) in binds.0.keys().zip(addrs.clone()) {
+                    next_locals.insert(var.clone(), Value::Addr(addr));
+                }
+
+                // 3. enable closures to access free variables
+                let locals_rhs = if *rec { &next_locals } else { locals };
+                for addr in addrs {
+                    let mut addr = addr.borrow_mut();
+                    let closure = addr.as_mut().unwrap();
+                    closure.free =
+                        ToValue::vals(&closure.lf.free, locals_rhs, &Default::default())?;
+                }
+
+                state.code = Code::Eval {
+                    expr,
+                    locals: next_locals,
+                };
+            }
+
+            Code::Eval {
+                expr: Expr::Case { expr, alts },
+                locals,
+            } => {
+                let locals = std::mem::take(locals);
+                state.code = Code::Eval {
+                    expr,
+                    locals: locals.clone(),
+                };
+                state.returns.push(ReturnStackFrame { alts, locals });
+            }
+
+            Code::Eval {
+                expr: Expr::ConstrApp { constr, args },
+                locals,
+            } => {
+                let args = ToValue::vals(args, locals, &state.globals)?;
+                state.code = Code::ReturnCon { constr, args };
+            }
+
+            Code::Eval {
+                expr: Expr::Lit(n), ..
+            } => {
+                state.code = Code::ReturnInt(*n);
+            }
+
+            Code::Eval {
+                expr: Expr::PrimApp { prim, args },
+                locals,
+            } => {
+                let n = match (
+                    &prim[..],
+                    ToValue::vals(args, locals, &Default::default())?.as_slice(),
+                ) {
+                    ("+#", [Value::Int(x), Value::Int(y)]) => x + y,
+                    ("-#", [Value::Int(x), Value::Int(y)]) => x - y,
+                    ("*#", [Value::Int(x), Value::Int(y)]) => x * y,
+                    ("/#", [Value::Int(x), Value::Int(y)]) => {
+                        if *y == 0 {
+                            return Err(DivisionByZero.into());
+                        } else {
+                            x / y
+                        }
+                    }
+                    (op, _) => return Err(UnknownPrimOp(op.to_owned()).into()),
+                };
+                state.code = Code::ReturnInt(n);
+            }
+
+            Code::Enter(addr) => {
+                let _addr = addr.borrow();
+                match &*_addr {
+                    Some(Closure {
+                        lf:
+                            LambdaForm {
+                                free: lf_free,
+                                updatable,
+                                args: lf_args,
+                                expr,
+                            },
+                        free,
+                    }) => {
+                        if *updatable {
+                            let args = std::mem::take(&mut state.args);
+                            let returns = std::mem::take(&mut state.returns);
+                            state.updates.push(UpdateStackFrame {
+                                args,
+                                returns,
+                                addr: Rc::clone(&addr),
+                            });
+                            let locals =
+                                lf_free.iter().cloned().zip(free.iter().cloned()).collect();
+                            drop(_addr);
+                            state.code = Code::Eval { expr, locals };
+                        } else {
+                            let args = state.args.drain(..lf_args.len());
+
+                            let arg_pairs = lf_args.iter().cloned().zip(args);
+                            let free_pairs = lf_free.iter().cloned().zip(free.iter().cloned());
+                            let locals = arg_pairs.chain(free_pairs).collect();
+                            drop(_addr);
+                            state.code = Code::Eval { expr, locals };
+                        }
+                    }
+
+                    None => return Err(EnterBlackhole.into()),
+                }
+            }
+
+            _ => return Ok(()),
+        };
     }
-
-    Ok(())
 }
 
 fn prepend<T>(dest: &mut Vec<T>, src: Vec<T>) {
@@ -135,177 +261,46 @@ fn prepend<T>(dest: &mut Vec<T>, src: Vec<T>) {
     dest.extend(tmp);
 }
 
-fn eval_var_app(state: &mut State) -> Result<bool> {
-    if let Code::Eval {
-        expr: Expr::VarApp {
-            var,
-            args: expr_args,
-        },
-        locals,
-    } = &state.code
-    {
-        if let (Value::Addr(addr), expr_args) = (
-            var.val(locals, &state.globals)?,
-            ToValue::vals(expr_args, locals, &state.globals)?,
-        ) {
-            state.code = Code::Enter(Rc::clone(&addr));
-            prepend(&mut state.args, expr_args);
-            Ok(true)
-        } else {
-            Ok(false)
-        }
-    } else {
-        Ok(false)
+#[derive(Debug)]
+struct UnknownPrimOp(String);
+
+impl fmt::Display for UnknownPrimOp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Unknown primitive operation: {}", self.0)
     }
 }
 
-fn enter_non_updatable(state: &mut State) -> Result<bool> {
-    if let Code::Enter(addr) = &state.code {
-        let addr = addr.borrow();
-        match &*addr {
-            Some(Closure {
-                lf:
-                    LambdaForm {
-                        free: lf_free,
-                        updatable: true,
-                        args: lf_args,
-                        expr,
-                    },
-                free,
-            }) if state.args.len() >= lf_args.len() => {
-                let args = state.args.drain(..lf_args.len());
+impl Error for UnknownPrimOp {}
 
-                let arg_pairs = lf_args.iter().cloned().zip(args);
-                let free_pairs = lf_free.iter().cloned().zip(free.iter().cloned());
-                let locals = arg_pairs.chain(free_pairs).collect();
-                drop(addr);
-                state.code = Code::Eval { expr, locals };
+#[derive(Debug)]
+struct DivisionByZero;
 
-                Ok(true)
-            }
-            _ => Ok(false),
-        }
-    } else {
-        Ok(false)
+impl fmt::Display for DivisionByZero {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Division by zero")
     }
 }
 
-fn eval_let(state: &mut State) -> Result<bool> {
-    match &state.code {
-        Code::Eval {
-            expr: Expr::Let { rec, binds, expr },
-            locals,
-        } => {
-            // 1. allocate closures
-            let addrs = binds
-                .0
-                .values()
-                .map(|lf| Rc::new(RefCell::new(Some(Closure { lf, free: vec![] }))));
+impl Error for DivisionByZero {}
 
-            // 2. create bindings
-            let mut next_locals = locals.clone();
-            for (var, addr) in binds.0.keys().zip(addrs.clone()) {
-                next_locals.insert(var.clone(), Value::Addr(addr));
-            }
+#[derive(Debug)]
+struct EnterBlackhole;
 
-            // 3. enable closures to access free variables
-            let locals_rhs = if *rec { &next_locals } else { locals };
-            for addr in addrs {
-                let mut addr = addr.borrow_mut();
-                let closure = addr.as_mut().unwrap();
-                closure.free = ToValue::vals(&closure.lf.free, locals_rhs, &Default::default())?;
-            }
-
-            state.code = Code::Eval {
-                expr,
-                locals: next_locals,
-            };
-            Ok(true)
-        }
-        _ => Ok(false),
+impl fmt::Display for EnterBlackhole {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Enter blackhole")
     }
 }
 
-fn eval_case(state: &mut State) -> Result<bool> {
-    match &mut state.code {
-        Code::Eval {
-            expr: Expr::Case { expr, alts },
-            locals,
-        } => {
-            let locals = std::mem::take(locals);
-            state.code = Code::Eval {
-                expr,
-                locals: locals.clone(),
-            };
-            state.returns.push(ReturnStackFrame { alts, locals });
-            Ok(true)
-        }
-        _ => Ok(false),
+impl Error for EnterBlackhole {}
+
+#[derive(Debug)]
+struct ApplyToInt;
+
+impl fmt::Display for ApplyToInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Arguments are applied to integer")
     }
 }
 
-fn eval_constr_app(state: &mut State) -> Result<bool> {
-    match &state.code {
-        Code::Eval {
-            expr: Expr::ConstrApp { constr, args },
-            locals,
-        } => {
-            let args = ToValue::vals(args, locals, &state.globals)?;
-            state.code = Code::ReturnCon { constr, args };
-            Ok(true)
-        }
-        _ => Ok(false),
-    }
-}
-
-fn eval_literal(state: &mut State) -> Result<bool> {
-    if let Code::Eval {
-        expr: Expr::Lit(n), ..
-    } = &state.code
-    {
-        state.code = Code::ReturnInt(*n);
-        Ok(true)
-    } else {
-        Ok(false)
-    }
-}
-
-fn eval_literal_app(state: &mut State) -> Result<bool> {
-    if let Code::Eval {
-        expr: Expr::VarApp { var, args },
-        locals,
-    } = &state.code
-    {
-        if let (Value::Int(n), true) = (var.val(locals, &state.globals)?, args.is_empty()) {
-            state.code = Code::ReturnInt(n);
-            Ok(true)
-        } else {
-            Ok(false)
-        }
-    } else {
-        Ok(false)
-    }
-}
-
-fn eval_prim_app(state: &mut State) -> Result<bool> {
-    if let Code::Eval {
-        expr: Expr::PrimApp { prim, args },
-        locals,
-    } = &state.code
-    {
-        let n = match (
-            &prim[..],
-            ToValue::vals(args, locals, &Default::default())?.as_slice(),
-        ) {
-            ("+#", [Value::Int(x), Value::Int(y)]) => x + y,
-            ("-#", [Value::Int(x), Value::Int(y)]) => x - y,
-            ("*#", [Value::Int(x), Value::Int(y)]) => x * y,
-            ("/#", [Value::Int(x), Value::Int(y)]) => x / y,
-            _ => return Ok(false),
-        };
-        state.code = Code::ReturnInt(n);
-        Ok(true)
-    } else {
-        Ok(false)
-    }
-}
+impl Error for ApplyToInt {}
